43a22a91edc1b26797ebdc99196fbc12ae2094bc
gavintjhxx <gavintjhx@gmail.com>
2021-06-12 15:17:31 +0800

    [UPDATE] Editions to versioning and existing features

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # **Clash Royale Node.js Library**
 
-**Latest Version:** 1.0.0
+**Latest Version:** 2.0.0
 
 # **Documentation**
 Install RoyaleJS | `npm i @zloth/royalejs`<br>
@@ -10,30 +10,36 @@ Changelogs in versions and updates can be found in [/changelog.md](https://githu
 ## **Existing Features:**
 * Clans
   * Get information of a specific clan
-  * Get Members in a specific clan
-  * Get Current River Race of a specific clan
-  * Get Current War of a specific clan
-  * Get River Race Log of a specific clan
-  * Get War Log of a specific clan
+    * Get Members 
+    * Get Current River Race 
+    * Get Current War 
+    * Get River Race Log 
+    * Get War Log
 * Players
   * Get information of a specific player
-  * Get Upcoming Chests of a specific player
-  * Get Battle Log of a specific player
+    * Get Upcoming Chests 
+    * Get Battle Log 
 * Cards
   * Get a full list and information of all existing cards
 * Tournaments
   * Get information of a specific tournament
-  * Get a full list and information of all global tournaments
 * Locations
   * Get a full list and information of all existing locations
   * Get information of a specific location
-  * Get Player Rankings in a specific location
-  * Get Clan Rankings in a specific location
-  * Get Clan War Rankings in a specific location
-  * Get full list and information of Top Player League seasons
-  * Get information of a specific Top Player League season
-  * Get full list and information of Top Player Rankings in a specific season
+
+## Upcoming features
+* 2.0.1
+  * Get a full list of Player Rankings in a specific location
+  * Get a full list of Clan Rankings in a specific location
+  * Get a full list of Clan War Rankings in a specific location
   * Get Global Tournament Ranking of a specific tournament
+* 2.0.2
+  * Get a full list of Top Player Rankings in a specific Season
+* 2.0.3
+  * Get a full list and information of all existing Clans
+  * Get a full list and information of all existing Tournaments
+  * Get a full list and information of all existing Global Tournaments
+  * Get a a full list and information of all existing Top Player League Seasons
 
 ## **Configuration**
 ```javascript
@@ -48,8 +54,9 @@ client.getClan("clanTag").then((clan) => {
   // Do something
 });
 
-client.getPlayer("playerTag").then((player) => {
-  console.log(player);
-  // Do something
-});
+// OR 
+
+let clan = await client.getClan("clantag");
+console.log(clan.members[0]);
+// Do something
 ```
--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,6 +1,6 @@
 const baseURL = "https://api.clashroyale.com/v1";
 
-module.exports.CLANS = () => `${baseURL}/clans` // Unverified, Await V1.0.2
+module.exports.CLANS = () => `${baseURL}/clans` // Unverified, Await V2.0.3
 module.exports.CLAN = (clanTag) => `${baseURL}/clans/%23${clanTag}`; // Clan
 
 /* Comments will indicate Class used for object returned */
@@ -10,15 +10,15 @@ module.exports.PLAYER_BATTLE_LOG = (playerTag) => `${baseURL}/players/%23${playe
 
 module.exports.CARDS = () => `${baseURL}/cards`; // Cards
 
-module.exports.TOURNAMENTS = () => `${baseURL}/tournaments`; // Unverified, Await V1.0.2
+module.exports.TOURNAMENTS = () => `${baseURL}/tournaments`; // Unverified, Await V2.0.3
 module.exports.TOURNAMENT = (tournamentTag) => `${baseURL}/tournaments/%23${tournamentTag}`; // Tournament
-module.exports.GLOBALTOURNAMENTS = () => `${baseURL}/globaltournaments`; // Unverified, Await V1.0.2
+module.exports.GLOBALTOURNAMENTS = () => `${baseURL}/globaltournaments`; // Unverified, Await V2.0.3
 
 module.exports.LOCATIONS = () => `${baseURL}/locations`; // Locations
 module.exports.LOCATION = (locationID) => `${baseURL}/locations/${locationID}`; // Location
-module.exports.PLAYER_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/players`; // Await V1.0.1
-module.exports.CLAN_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/clans`; // Await V1.0.1
-module.exports.CLAN_WAR_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/clanwars`; // Await V1.0.1
-module.exports.TOP_PLAYER_LEAGUE_SEASONS = () => `${baseURL}/locations/global/seasons`; // Await V1.0.1
-module.exports.TOP_PLAYER_RANKINGS_IN_SEASON = (seasonID) => `${baseURL}/locations/global/seasons/${seasonID}/rankings/players`; // Await V1.0.1
-module.exports.GLOBAL_TOURNAMENT_RANKINGS = (tournamentTag) => `${baseURL}/locations/global/rankings/tournaments/${tournamentTag}` // Await V1.0.1
+module.exports.PLAYER_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/players`; // Await V2.0.1
+module.exports.CLAN_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/clans`; // Await V2.0.1
+module.exports.CLAN_WAR_RANKINGS_BY_LOCATION = (locationID) => `${baseURL}/locations/${locationID}/rankings/clanwars`; // Await V2.0.1
+module.exports.TOP_PLAYER_LEAGUE_SEASONS = () => `${baseURL}/locations/global/seasons`; // Await V2.0.3
+module.exports.TOP_PLAYER_RANKINGS_IN_SEASON = (seasonID) => `${baseURL}/locations/global/seasons/${seasonID}/rankings/players`; // Await V2.0.2
+module.exports.GLOBAL_TOURNAMENT_RANKINGS = (tournamentTag) => `${baseURL}/locations/global/rankings/tournaments/${tournamentTag}` // Await V2.0.1
